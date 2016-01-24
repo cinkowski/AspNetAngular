8ac89e7af8992ed7934efc1a31ecbe25d6a3aef2
cinkulol <mmarczakm@gmail.com>
2016-01-24 16:01:24 +0100

    initial commit

---
+++ AspNetAngular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetAngular.Startup))]
+namespace AspNetAngular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
