429fa771555a05ba0249dff9de1826c966d9241f
cinkowski <cinq2ez@gmail.com>
2016-01-26 20:00:41 +0100

    Add basic routing, Add login/register views/logic

--- AspNetAngular/App_Start/BundleConfig.cs
+++ AspNetAngular/App_Start/BundleConfig.cs
@@ -15,8 +15,13 @@ namespace AspNetAngular
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+            bundles.Add(new ScriptBundle("~/bundles/Angular")
+                .Include("~/Scripts/angular.min.js")
+                .Include("~/Scripts/angular-route.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/AspNetAngular")
                 .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .IncludeDirectory("~/Scripts/Factories", "*.js")
                 .Include("~/Scripts/AspNetAngular.js"));
 
             BundleTable.EnableOptimizations = true;
--- AspNetAngular/Scripts/AspNetAngular.js
+++ AspNetAngular/Scripts/AspNetAngular.js
@@ -1,3 +1,35 @@
-﻿var AspNetAngular = angular.module('AspNetAngular', []);
+﻿var AspNetAngular = angular.module('AspNetAngular', ['ngRoute']);
 
 AspNetAngular.controller('LandingPageController', LandingPageController);
+AspNetAngular.controller('LoginController', LoginController);
+AspNetAngular.controller('RegisterController', RegisterController);
+
+AspNetAngular.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
+AspNetAngular.factory('LoginFactory', LoginFactory);
+AspNetAngular.factory('RegistrationFactory', RegistrationFactory);
+
+var configFunction = function ($routeProvider, $httpProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo/:donuts', {
+            templateUrl: function (params) { return 'routesDemo/two?donuts=' + params.donuts; }
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        })
+        .when('/login', {
+            templateUrl: '/Account/Login',
+            controller: LoginController
+        })
+        .when('/register', {
+            templateUrl: '/Account/Register',
+            controller: RegisterController
+        });
+
+    $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
+}
+configFunction.$inject = ['$routeProvider', '$httpProvider'];
+
+AspNetAngular.config(configFunction);
